88425d218d6bfa7317dd659a23572e196275fbd9
Jonathan Dupré <hello@jonathandupre.com>
2018-01-28 16:38:35 -0500

    implement basic query matching when fetching items
    - pass storage obj when instantiating Library
    - filter items() over given query (basic implementation with indexOf)
    - split existing tests in two

--- test/index.js
+++ test/index.js
@@ -1,36 +1,68 @@
 var path = require('path')
 var test = require('tape')
 var rimraf = require('rimraf')
+var series = require('run-series')
+
 var Item = require('../lib/Item')
 var Library = require('../lib/Library')
 
-rimraf.sync(path.join(__dirname, '../vibd.db'))
-
-var vibd = Library()
+const storage = path.join(__dirname, './vibd.db')
 
 test('add item & retrieve it', function (t) {
+  rimraf.sync(storage)
+
+  var vibd = Library(storage)
   var item = Item()
 
   item.id = '1234'
 
-  vibd.add(item, function (err, id) {
-    t.equal(err, null, 'library#add: no error')
+  series([ add, get ], t.end)
 
-    vibd.get(id, onGet)
-  })
+  function add (done) {
+    vibd.add(item, function (err, id) {
+      t.equal(err, null, 'library#add: no error')
 
-  function onGet (err, item) {
-    t.equal(err, null, 'library#get: no error')
-    t.equal(item instanceof Item, true, 'returned item is of right type')
-
-    vibd.items('query', 'sort', onItems)
+      done()
+    })
   }
 
-  function onItems (err, items) {
-    t.equal(err, null, 'library#items: no error')
-    t.ok(Array.isArray(items), 'library#items: result is an array')
-    t.ok(items.every(item => item instanceof Item), 'library#items: returns array of Items')
+  function get (done) {
+    vibd.get(item.id, function (err, item) {
+      t.equal(err, null, 'library#get: no error')
+      t.equal(item instanceof Item, true, 'returned item is of right type')
 
-    t.end()
+      done()
+    })
+  }
+})
+
+test('add items & retrieve them', function (t) {
+  rimraf.sync(storage)
+
+  var vibd = Library(storage)
+  var item = Item()
+
+  item.id = '1234'
+
+  series([ add, items ], t.end)
+
+  function add (done) {
+    vibd.add(item, function (err, id) {
+      t.equal(err, null, 'library#add: no error')
+
+      done()
+    })
+  }
+
+  function items (done) {
+    var query = ''
+
+    vibd.items(query, function items (err, items) {
+      t.equal(err, null, 'library#items: no error')
+      t.ok(Array.isArray(items), 'library#items: result is an array')
+      t.ok(items.every(item => item instanceof Item), 'library#items: returns array of Items')
+
+      done()
+    })
   }
 })
